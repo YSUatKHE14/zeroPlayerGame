7a2c01e8f73b96b6a3c5b5fe35b0d6ca3513288b
Joe Duncko <dotmaners@gmail.com>
2014-10-25 11:22:30 -0400

    Initialized files

---
+++ main.js
@@ -0,0 +1,13 @@
+//take variables from from
+
+//initialize character
+
+function play() {
+	//pathFunction1();
+	//pathFunction2();
+	//pathFunction3();
+	//pathFunction4();
+	//pathFunction5();
+
+	//finalBoss();
+}
---
+++ pathFunctions.js
@@ -0,0 +1,23 @@
+function pathfunction1() {
+
+}
+
+function pathfunction2() {
+	
+}
+
+function pathfunction3() {
+	
+}
+
+function pathfunction4() {
+	
+}
+
+function pathfunction5() {
+	
+}
+
+function finalBoss() {
+	
+}
