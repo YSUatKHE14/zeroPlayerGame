6fb4ee7fd9c6c6b1bff68386be0fdedfaea1cc5f
capalmer1013 <capalmer1013@gmail.com>
2014-10-25 13:30:19 -0400

    Begining bandit attack stuff

--- js/pathFunctions.js
+++ js/pathFunctions.js
@@ -31,7 +31,25 @@ var wolf = new Character();
 }
 
 function banditsAttack() {
-
+	var bandit = new Character();
+		bandit.health = 40;
+		bandit.attack = 10;
+		bandit.defense = 5;
+	while(wolf.health > 0 && hero.health > 0)
+	{
+		var chance = Math.floor((Math.random() * 100) + 1);
+		if(chance < 20){
+			console.log(hero.health);
+			generalAttack(wolf, hero);
+			console.log("wolf has attacked");
+			console.log(hero.health);
+		}
+		else {
+			console.log(wolf.health);
+			generalAttack(hero, wolf);
+			console.log("hero has attacked");
+			console.log(wolf.health);
+		}
 }
 
 function trollsAttack() {
