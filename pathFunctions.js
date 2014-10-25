43c040f24565e5680719102efa745e7dba180151
capalmer1013 <capalmer1013@gmail.com>
2014-10-25 12:01:23 -0400

    wolf attack function
    lets see if this thang works

--- pathFunctions.js
+++ pathFunctions.js
@@ -1,5 +1,26 @@
+function generalAttack(attacker, receiver){
+	if(attacker.attack > receiver.defense){
+		receiver.health = receiver.health - (receiver.defence - attacker.attack);
+	}
+}
 function wolvesAttack() {
-
+var wolf = {
+		health: 30,
+		attack: 5,
+		defense: 5
+	}
+	while(wolf.health > 0 && hero.health > 0)
+	{
+		var chance = Math.floor((Math.random() * 100) + 1);
+		if(chance < 20){
+			generalAttack(wolf, hero);
+			console.log("wolf has attacked");
+		}
+		else {
+			generalAttack(hero, wolf);
+			console.log("hero ahs attacked");
+		}
+	}
 }
 
 function banditsAttack() {
@@ -15,7 +36,8 @@ function golemsAttack() {
 }
 
 function dragonAttacks() {
-	
+// atk 30
+// 
 }
 
 function blackSquirrelAttacks() {
