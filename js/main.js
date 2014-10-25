4ebf0f6bca9973ce3fe6a59a0c07e826ee796b00
Patrick Bollinger <pjbollinger@student.ysu.edu>
2014-10-25 12:30:25 -0400

    Moved main.js to js folder.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,40 @@
 //take variables from from
 var hero = {
+  name: "Hero",
   health: 100,
   attack: 10,
   defense: 10
 }
 
 //initialize character
-function initialize_character () {
-  hero.health = 100 // document.getElementById("health").value
-  hero.attack = 10 // document.getElementById("attack").value
-  hero.defense = 10 // document.getElementById("defense").value
+function initialize_character() {
+  hero.name = document.getElementById('fname');
+  hero.health = 100; // Starting health
+  hero.attack = 10; // document.getElementById("attack").value
+  hero.defense = 10; // document.getElementById("defense").value
 }
 
-function play() {
-	wolvesAttack();  // Do the thing
-	//pathFunction2();
-	//pathFunction3();
-	//pathFunction4();
-	//pathFunction5();
+function switch_to_gameplay() {
+  document.getElementById('initialization').style.display = "none";
+  document.getElementById('gameplay').style.display = "block";
+}
 
-	//finalBoss();
+function update_status() {
+  // document.getElementById("health").value = hero.health
+}
+
+function gameplay() {
+  //pathFunction1();
+  //pathFunction2();
+  //pathFunction3();
+  //pathFunction4();
+  //pathFunction5();
+
+  //finalBoss();
+}
+
+function run () {
+  initialize_character();
+  switch_to_gameplay();
+  gameplay();
 }
