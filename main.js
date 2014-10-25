aefb7c538c4f30da7bc51a1b18f5c12613565108
Patrick Bollinger <pjbollinger@student.ysu.edu>
2014-10-25 12:26:18 -0400

    Trying to merge with @capalmer1014 change

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 //take variables from from
 var hero = {
+  name: "Hero",
   health: 100,
   attack: 10,
   defense: 10
 }
 
 //initialize character
-function initialize_character () {
-  hero.health = 100 // document.getElementById("health").value
-  hero.attack = 10 // document.getElementById("attack").value
-  hero.defense = 10 // document.getElementById("defense").value
+function initialize_character() {
+  hero.name = document.getElementById('fname');
+  hero.health = 100; // Starting health
+  hero.attack = 10; // document.getElementById("attack").value
+  hero.defense = 10; // document.getElementById("defense").value
 }
 
-function play() {
+function switch_to_gameplay() {
+  document.getElementById('initialization').style.display = "none";
+  document.getElementById('gameplay').style.display = "block";
+}
+
+function update_status() {
+  // document.getElementById("health").value = hero.health
+}
+
+function gameplay() {
 	//pathFunction1();
 	//pathFunction2();
 	//pathFunction3();
@@ -20,4 +31,10 @@ function play() {
 	//pathFunction5();
 
 	//finalBoss();
+}
+
+function run () {
+  initialize_character();
+  switch_to_gameplay();
+  gameplay();
 }
