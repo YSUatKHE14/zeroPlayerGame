b282a4a8c4221e73fdc0902df3fdaf91684aff65
capalmer1013 <capalmer1013@gmail.com>
2014-10-27 17:51:56 -0400

    Adding code for Inventory Items
    Included the inventoryItems.js file which will include functions for
    each of the items to be chosen at the begining.

---
+++ js/inventoryItems.js
@@ -0,0 +1,26 @@
+// Functions to implement effects of items which will need to be added to the UI
+// I'd say we should have a checkbox full of ~10 items where the player can pick about 3
+// Shield can be implemented with this
+function useItem(){
+}
+
+function healthPotion(){
+}
+function shield(){
+}
+function rabidToad(){
+}
+function bigMac(){
+}
+function theLiteralHeartOfALion(){
+}
+function soupThatIsTooHot(){
+}
+function throwingStar(){
+}
+function poison(){
+}
+function aBucketOfChildrensTears(){
+}
+function theLastKnownBison(){
+}
