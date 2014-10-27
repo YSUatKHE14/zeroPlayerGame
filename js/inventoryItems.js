d22ea5450b6914255b6e3a7c22a80bc4ba36f4fc
capalmer1013 <capalmer1013@gmail.com>
2014-10-27 18:41:15 -0400

    I guess that last one wasn't the switch statement

--- js/inventoryItems.js
+++ js/inventoryItems.js
@@ -1,7 +1,41 @@
 // Functions to implement effects of items which will need to be added to the UI
 // I'd say we should have a checkbox full of ~10 items where the player can pick about 3
 // Shield can be implemented with this
-function useItem(){
+function useItem(hero){
+	for(var i = 0; i < 3; i++){
+		switch(hero.items[i]){
+			case "healthPotion":
+				healthPotion(hero);
+				break;
+			case "shield":
+				shield(hero);
+				break;
+			case "rabidToad":
+				rabidToad(hero);
+				break;
+			case "bigMac":
+				bigMac(hero);
+				break;
+			case "theLiteralHeartOfALion":
+				theLiteralHeartOfALion(hero);
+				break;
+			case "soupThatIsTooHot":
+				soupThatIsTooHot(hero);
+				break:
+			case "throwingStar":
+				throwingStar(hero);
+				break;
+			case "poison":
+				poison(hero);
+				break;
+			case "aBucketOfChildrensTears":
+				aBucketOfChildrensTears(hero);
+				break;
+			case "theLastKnownBison":
+				theLastKnownBison(hero);
+				break;
+		}
+	}
 }
 
 function healthPotion(){
