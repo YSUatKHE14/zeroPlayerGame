cd06cd2b969849c4c9c60c050fa31e1924171e7e
capalmer1013 <capalmer1013@gmail.com>
2014-10-27 23:35:54 -0400

    read description for the details yo!
    index.html
    -added class inventory to html
    main.js
    -cleaned some code
    -working on storing checkboxes to hero.inventory variable

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ function Character() {
   this.health = 100;
   this.attack = 10;
   this.defense = 10;
-  this.items = [];
 }
 var hero = new Character();
 
@@ -15,6 +14,16 @@ function initialize_character() {
   hero.attack = document.getElementById('stats').value; // document.getElementById("attack").value
   hero.defense = 10 - document.getElementById('stats').value; // document.getElementById("defense").value
   hero.weapon = document.getElementById('weapon').value; //get chosen weapon
+  var checkedValue = null; 
+  hero.inventory = document.querySelector('.inventory:checked').value;
+  for(var i=0; hero.inventory[i]; ++i){
+        if(hero.inventory[i].checked){
+             checkedValue = inputElements[i].value;
+             break;
+        }
+  }
+  console.log(hero.inventory);
+  console.log(checkedValue);
 }
 
 function switch_to_gameplay() {
@@ -51,6 +60,7 @@ function showResults() {
 
 function run () {
   initialize_character();
+  console.log(hero);
   switch_to_gameplay();
   gameplay();
   showResults();
